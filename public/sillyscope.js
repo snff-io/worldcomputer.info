2ef0dfc6fd5daf50f54792c844f6f4a47eca0bfb
Joshua Zampino <josh@worldcomputer.info>
2024-01-25 11:19:14 -0500

    halp, my laptop is hungry!

--- public/sillyscope.js
+++ public/sillyscope.js
@@ -1,10 +1,11 @@
 
 audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
-function draw(analyser) {
+function draw() {
 
-    requestAnimationFrame(()=>{draw(analyser)});
-    analyser.getByteTimeDomainData(analyser.dataArray);
+    requestAnimationFrame(draw);
+    ana.dataArray = new Uint8Array(analyser.frequencyBinCount);
+    ana.getByteTimeDomainData(analyser.dataArray);
 
     canvas = document.getElementById('sillyscope');
     cc = canvas.getContext('2d');
@@ -18,12 +19,12 @@ function draw(analyser) {
     cc.strokeStyle = 'rgb(45,118, 147)';
     cc.beginPath();
 
-    var dataArrayLength = analyser.dataArray.length;
+    var dataArrayLength = ana.dataArray.length;
     var sliceWidth = (WIDTH * 1.0) / dataArrayLength;
     
     var x = 0;
     for (var i = 0; i < dataArrayLength; i++) {
-        var v = analyser.dataArray[i] / 128.0;
+        var v = ana.dataArray[i] / 128.0;
         var y = (v * HEIGHT) / 2;
         if (i === 0) {
             cc.moveTo(x, y);
@@ -38,16 +39,15 @@ function draw(analyser) {
     
 }
 
-osc = audioContext.createOscillator();
-osc.frequency.value = 440; // Set the frequency to 440 Hz (A4)
+const osc = audioContext.createOscillator();
+osc.frequency.value = 600; // Set the frequency to 440 Hz (A4)
 osc.type = 'sine'; // Set the waveform type to sine
 
-ana = audioContext.createAnalyser();
+const ana = audioContext.createAnalyser();
 bufferSize = ana.frequencyBinCount;
 ana.dataArray = new Uint8Array(bufferSize);
 
 osc.connect(ana);
 
 osc.start();
-
-draw(ana);
+draw();
